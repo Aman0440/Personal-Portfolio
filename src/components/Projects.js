a0c04c8a0296028c65219d51e6ac4e7864ed5e09
Aman Shaikh <amanshaikh9156@gmail.com>
2026-03-08 09:49:44 +0530

    Fix Projects alt attribute

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,15 +4,15 @@ import projImg1 from "../assets/img/portfolio.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
-import 'animate.css';
-import TrackVisibility from 'react-on-screen';
+import "animate.css";
+import TrackVisibility from "react-on-screen";
 
 export const Projects = () => {
-
   const projects = [
     {
-      title: "PortFolio",
-      description: "Personal Portfilio  , Developed a responsive personal portfolio website to showcase my projects, skills, and contact information. The website features a modern UI design, smooth navigation, and interactive sections such as About, Skills, Projects, and Contact. Implemented responsive layouts to ensure compatibility across different devices and screen sizes.\nTechnologies Used:HTML, CSS, JavaScript, React.js",
+      title: "Portfolio",
+      description:
+        "Developed a responsive personal portfolio website to showcase my projects, skills, and contact information. The website features a modern UI design, smooth navigation, and interactive sections such as About, Skills, Projects, and Contact. Implemented responsive layouts to ensure compatibility across different devices and screen sizes. Technologies Used: HTML, CSS, JavaScript, React.js",
       imgUrl: projImg1,
     },
     {
@@ -48,49 +48,68 @@ export const Projects = () => {
         <Row>
           <Col size={12}>
             <TrackVisibility>
-              {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
-                <h2>Projects</h2>
-                <p>Here are some of the key projects I’ve worked on, showcasing my skills in full-stack development and problem-solving.</p>
-                <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                    <Nav.Item>
-                      <Nav.Link eventKey="first">Projeect 1 </Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="second">Project 2 </Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="third">Project 3</Nav.Link>
-                    </Nav.Item>
-                  </Nav>
-                 <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
-  <Tab.Pane eventKey="first">
-    <Row>
-      <ProjectCard {...projects[0]} />
-    </Row>
-  </Tab.Pane>
+              {({ isVisible }) => (
+                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                  <h2>Projects</h2>
+                  <p>
+                    Here are some of the key projects I’ve worked on, showcasing my
+                    skills in full-stack development and problem-solving.
+                  </p>
 
-  <Tab.Pane eventKey="second">
-    <Row>
-      <ProjectCard {...projects[1]} />
-    </Row>
-  </Tab.Pane>
+                  <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                    <Nav
+                      variant="pills"
+                      className="nav-pills mb-5 justify-content-center align-items-center"
+                      id="pills-tab"
+                    >
+                      <Nav.Item>
+                        <Nav.Link eventKey="first">Project 1</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="second">Project 2</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="third">Project 3</Nav.Link>
+                      </Nav.Item>
+                    </Nav>
 
-  <Tab.Pane eventKey="third">
-    <Row>
-      <ProjectCard {...projects[2]} />
-    </Row>
-  
-                    </Tab.Pane>
-                  </Tab.Content>
-                </Tab.Container>
-              </div>}
+                    <Tab.Content
+                      id="slideInUp"
+                      className={
+                        isVisible ? "animate__animated animate__slideInUp" : ""
+                      }
+                    >
+                      <Tab.Pane eventKey="first">
+                        <Row>
+                          <ProjectCard {...projects[0]} />
+                        </Row>
+                      </Tab.Pane>
+
+                      <Tab.Pane eventKey="second">
+                        <Row>
+                          <ProjectCard {...projects[1]} />
+                        </Row>
+                      </Tab.Pane>
+
+                      <Tab.Pane eventKey="third">
+                        <Row>
+                          <ProjectCard {...projects[2]} />
+                        </Row>
+                      </Tab.Pane>
+                    </Tab.Content>
+                  </Tab.Container>
+                </div>
+              )}
             </TrackVisibility>
           </Col>
         </Row>
       </Container>
-      <img className="background-image-right" src={colorSharp2}></img>
+
+      <img
+        className="background-image-right"
+        src={colorSharp2}
+        alt=""
+      />
     </section>
-  )
-}
+  );
+};
